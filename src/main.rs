55fa423848f66f05d9266e6a7d041d1a261c1a6f
Jack <jack@Jacks-MacBook-Pro.local>
2023-10-03 17:05:55 +0530

    wifi status check

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use std::process::Command;
 
-fn main() {
+fn is_wifi_on() -> bool {
     let output = Command::new("ifconfig")
         .output()
         .expect("Failed to execute ifconfig command");
 
     let stdout = String::from_utf8(output.stdout).expect("Failed to convert stdout to string");
 
-    if stdout.contains("wlan0") {
+    stdout.contains("wlan0")
+}
+
+fn main() {
+    let is_wifi_on = is_wifi_on();
+
+    if is_wifi_on {
         println!("WiFi is on.");
     } else {
         println!("WiFi is off.");
